b9042d8bc58f6f91b01505a415e646ff2dd48c7f
mirnas1 <103143261+mirnas1@users.noreply.github.com>
2024-08-03 10:07:47 -0400

    weather adjustments

---
+++ PythonScripts/set-rain.py
@@ -0,0 +1,34 @@
+import carla
+
+def change_weather():
+    client = carla.Client('localhost', 2000)
+    client.set_timeout(10.0)
+    world = client.get_world()
+
+    # Set weather conditions
+    weather = carla.WeatherParameters(
+        cloudiness=60.0,
+        precipitation=75.0,
+        precipitation_deposits=65.0,
+        wind_intensity=50.0,
+        sun_azimuth_angle=180.0,
+        sun_altitude_angle=70.0,
+        fog_density=0.1,
+        fog_distance=0.1,
+        wetness=0.85
+    )
+    """weather = carla.WeatherParameters(
+        cloudiness=100.0,
+        precipitation=0.0,
+        precipitation_deposits=0.0,
+        wind_intensity=50.0,
+        sun_azimuth_angle=180.0,
+        sun_altitude_angle=70.0,
+        fog_density=0.1,
+        fog_distance=0.1,
+        wetness=0.0
+    )"""
+    world.set_weather(weather)
+
+if __name__ == '__main__':
+    change_weather()
